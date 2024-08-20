a022651b18cfb1ab5d8a6e85d57fe4b378337df5
Wenchong Hu <whu@lyft.com>
2024-08-19 23:28:02 -0700

    Update site-package to python 3.10

--- lambda_compile.sh
+++ lambda_compile.sh
@@ -4,5 +4,5 @@ yum install -y python310
 python3.10 -m venv /tmp/venv
 /tmp/venv/bin/pip install --upgrade pip setuptools
 /tmp/venv/bin/pip install -e .
-cp -r /tmp/venv/lib/python3.7/site-packages/. ./aws_lambda_libs
-cp -r /tmp/venv/lib64/python3.7/site-packages/. ./aws_lambda_libs
+cp -r /tmp/venv/lib/python3.10/site-packages/. ./aws_lambda_libs
+cp -r /tmp/venv/lib64/python3.10/site-packages/. ./aws_lambda_libs
